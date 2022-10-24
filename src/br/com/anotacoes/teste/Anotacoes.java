5a3e944280d08b794eed02d4842184b356ed221f
Gabriel Sene <sene300@gmail.com>
2022-10-24 19:35:54 -0300

    anotações

---
+++ src/br/com/anotacoes/teste/Anotacoes.java
@@ -0,0 +1,22 @@
+package br.com.anotacoes.teste;
+
+public class Anotacoes {
+	public static void main(String[] args) {
+		/**
+		 * @author Gabriel
+		 *version 0.1
+		 *Classe teste, para guardar minhas anotações java
+		 */
+		
+		
+		System.out.println("public = publico");
+		System.out.println("---------------------------------");
+		System.out.println("protected, é visível dentro do pacote, público para as classes filhas");
+		System.out.println("---------------------------------");
+		System.out.println("vazio, exemplo double numero = publico apenas dentro do pacote");
+		System.out.println("---------------------------------");
+		System.out.println("private = é visível apenas dentro da classe");
+		
+	}
+
+}
